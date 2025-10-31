9c1bb24c1e0a1cdc9c2dcdc3b3f2c41ee55b05da
zhouun <89436419+zhouun@users.noreply.github.com>
2025-10-31 13:48:22 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -16,5 +16,5 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 #echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 #git clone https://github.com/kenzok8/small.git package/small
-git clone https://github.com/SunBK201/UA3F.git package/UA3F
-git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
+#git clone https://github.com/SunBK201/UA3F.git package/UA3F
+#git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
