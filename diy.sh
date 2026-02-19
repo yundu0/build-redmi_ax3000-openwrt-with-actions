e8af59c5850402a613fe811e4e8d3ad9809e8861
weihull <weihull@hotmail.com>
2026-02-19 23:11:51 +0800

    fix: clean up diy.sh, remove duplicate kiddin9 feed, fix theme sed timing

--- diy.sh
+++ diy.sh
@@ -1,21 +1,14 @@
-# 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
+#!/bin/bash
+# ============================================================
+# diy.sh - 自定义编译脚本
+# 此脚本在 feeds update 之前执行
+# ============================================================
 
-# 编辑默认的lan口ip地址
+# === 修改默认 LAN IP 地址 ===
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
-# 编辑默认的主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# 编辑默认的luci显示的固件名称
-#sed -i 's/OpenWrt/ZWRT/g' package/base-files/files/bin/config_generate
-#sed -i 's/ImmortalWrt/ZWRT/g' package/base-files/files/bin/config_generate
-
-# 添加额外的软件包，echo 方式和git clone 方式二选一即可
+# === 添加第三方软件包源 ===
+# 只使用 kenzok8 的两个源即可，包含 SSR Plus+、Passwall 等常用插件及其依赖
+# 注意：不要同时添加 kiddin9，会与 kenzok8 产生大量重复包导致冲突
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git kiddin9 https://github.com/kiddin9/kwrt-packages' >>feeds.conf.default
-#echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
-#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-#git clone https://github.com/kenzok8/small.git package/small
-#git clone https://github.com/SunBK201/UA3F.git package/UA3F
-#git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
