8996b0c832537d9b0c6c42cfeb4aea176f9f8717
weihull <weihull@hotmail.com>
2026-03-02 00:32:58 +0800

    feat: strictly apply kenzok8 upstream instructions to inject priority src-git and forcefully remove conflicting feed packages including golang before compiling

--- diy.sh
+++ diy.sh
@@ -8,7 +8,6 @@
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # === 添加第三方软件包源 ===
-# kenzok8/openwrt-packages: argon 主题、easymesh 等插件
-# kenzok8/small: 包含更新的各类代理软件及其依赖
-echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+# 优先使用 kenzok8 源，放在 feeds.conf.default 最前面覆盖默认源
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
