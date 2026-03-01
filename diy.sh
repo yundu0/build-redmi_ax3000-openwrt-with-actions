e2b7aa1b77f7e6084274b003c712d7ba73265cd7
weihull <weihull@hotmail.com>
2026-03-02 00:28:21 +0800

    feat: change package feed from helloworld to kenzok8/small and restore mosdns/xray-core

--- diy.sh
+++ diy.sh
@@ -9,6 +9,6 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 
 # === 添加第三方软件包源 ===
 # kenzok8/openwrt-packages: argon 主题、easymesh 等插件
-# fw876/helloworld: SSR Plus+ 原始维护仓库（更稳定，无循环依赖）
+# kenzok8/small: 包含更新的各类代理软件及其依赖
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
