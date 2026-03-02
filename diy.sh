4fc0bc405ae2cbde539337020a06701949ef4927
weihull <weihull@hotmail.com>
2026-03-02 17:20:04 +0800

    chore: revert to stable helloworld feed and disable conflicting packages for a clean build

--- diy.sh
+++ diy.sh
@@ -8,9 +8,10 @@
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # === 添加第三方软件包源 ===
-# 优先使用 kenzok8 源，放在 feeds.conf.default 最前面覆盖默认源
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# kenzok8/openwrt-packages: argon 主题、easymesh 等插件
+# fw876/helloworld: SSR Plus+ 原始维护仓库（更稳定，无循环依赖）
+echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 
 # 注意：高版本 golang (v1.26) 及其它冲突包（如 xray, v2ray, etc.）的替换，
 # 为了避免被 feeds update 恢复，统一在 Github Actions 工作流 (.github/workflows/) 
