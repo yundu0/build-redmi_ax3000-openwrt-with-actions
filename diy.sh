859d20b31d292c9f4ac64c44f20ccaf2fb128529
weihull <weihull@hotmail.com>
2026-05-22 18:49:32 +0800

    fix(redmi_ax3000): resolve ath11k-firmware package conflict and fix feed name mismatch

--- diy.sh
+++ diy.sh
@@ -9,7 +9,7 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 
 # === 添加第三方软件包源 ===
 # 优先使用 kenzok8 源，放在 feeds.conf.default 最前面覆盖默认源
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '1i src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # 注意：高版本 golang (v1.26) 及其它冲突包（如 xray, v2ray, etc.）的替换，
